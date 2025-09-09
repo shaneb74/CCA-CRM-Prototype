d01838178166ba5563f6dcd5ce611b55d5939795
shaneb74 <shaneb@conciergecareadvisors.com>
2025-09-09 17:34:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-
 import streamlit as st
 from ui.widgets import inject_css
-
-st.set_page_config(
-    page_title="Advisor Dashboard",
-    layout="wide",   # <-- this is the magic word
-    initial_sidebar_state="expanded"
-)
 
 st.set_page_config(page_title="Senior CRM Prototype", page_icon="📋", layout="wide")
 inject_css()
 
 st.markdown("# app")
-st.markdown("This is the landing page for initialization.  Click Dashbord on the left nav")
+st.markdown("Use the left nav to open pages.")
