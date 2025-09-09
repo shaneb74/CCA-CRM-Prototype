4503e70c11aa02b4af92da1eb8fd8c3b5f2f55bf
shaneb74 <shaneb@conciergecareadvisors.com>
2025-09-09 17:29:51 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,14 @@
 import streamlit as st
 from ui.widgets import inject_css
 
+import streamlit as st
+
+st.set_page_config(
+    page_title="Advisor Dashboard",
+    layout="wide",   # <-- this is the magic word
+    initial_sidebar_state="expanded"
+)
+
 st.set_page_config(page_title="Senior CRM Prototype", page_icon="📋", layout="wide")
 inject_css()
 
